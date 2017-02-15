da5950e4655ddbacacbf959deedb7b22c327d6b8
NajibOsman <machidydemajka38@gmail.com>
2017-02-15 15:07:08 +0300

    uppdates on js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var prompt_name=prompt("enter a sentence");
+
+
+ function capitalizeFirstLetter (string) {
+  return string && s [0].toUpperCase() + string.slice(1);
+}
+capitalizeFirstLetter(prompt_name);
+
+function reverse(s){
+  return s.split("").reverse().join("");
+}
+
+function callTo(capitalizeFirstLetter, reverse){
+  return
+}
